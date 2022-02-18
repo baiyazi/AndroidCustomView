dd4457ee810bd1fed5fc5ee5b0cfa1a67fbe65cb
baiyazi <1270563429@qq.com>
2022-02-18 20:49:58 +0800

    init project.

---
+++ app/src/main/java/com/weizu/myapplication/README.java
@@ -0,0 +1,9 @@
+package com.weizu.myapplication;
+
+/**
+ * 介绍：自定义View
+ * 时间：2022-2-18 20:40:13
+ * 作者：梦否
+ */
+public class README {
+}
---
+++ rotatemenu/src/main/java/com/weizu/rotatemenu/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.weizu.rotatemenu
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+/**
+ * 实现旋转菜单
+ */
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
